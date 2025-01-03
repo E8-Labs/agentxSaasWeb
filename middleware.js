4f6f5fac11cd45ca2588ef0fa41071145125fee9
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-01-04 03:29:12 +0500

    new test update

--- middleware.js
+++ middleware.js
@@ -1,31 +1,33 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-    // console.log("Trying middle ware");
-    // const localData = localStorage.getItem("User");
-    // if (!localData) {
     console.log("Running middleware");
 
     // Retrieve the user data from cookies
-
-    if (request.nextUrl.pathname == "/" || request.nextUrl.pathname == "/onboarding" || request.nextUrl.pathname == "/onboarding/WaitList") {
-
-    }
-    else {
+    if (request.nextUrl.pathname == "/" || request.nextUrl.pathname == "/onboarding") {
+        // No redirection needed for these paths
+    } else if (request.nextUrl.pathname == "/onboarding/waitlist") {
+        // Do not route the user from this specific path
+        return NextResponse.next();
+    } else {
         const userCookie = request.cookies.get("User");
         console.log("User Cookie:", userCookie);
         if (!userCookie) {
-            if (request.nextUrl.pathname != ["/", "/onboarding"]) {
-                return NextResponse.redirect(new URL("/", request.url))
-            }
+            // Redirect user to home page if they are not authenticated and not on allowed paths
+            return NextResponse.redirect(new URL("/", request.url));
         }
     }
 
-
-    // }
-
+    return NextResponse.next();  // Proceed if no conditions are met
 }
 
 export const config = {
-    matcher: ["/createagent/:path*", "/pipeline/:path*", "/sellerkycquestions/:path*", "/buyerkycquestions/:path*", "/dashboard/:path*", "/onboarding/:path*"]
+    matcher: [
+        "/createagent/:path*",
+        "/pipeline/:path*",
+        "/sellerkycquestions/:path*",
+        "/buyerkycquestions/:path*",
+        "/dashboard/:path*",
+        "/onboarding/:path*"
+    ]
 }
