76c9d19d2ceb4a1c5ce3323638e5424e0f35dc0f
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-01-27 03:29:28 +0500

    an update

--- components/dashboard/Navbar/ProfileNav.js
+++ components/dashboard/Navbar/ProfileNav.js
@@ -308,7 +308,9 @@ const ProfileNav = () => {
         if (
           // Data?.totalSecondsAvailable <= 120 ||
           Data?.plan == null ||
-          (Data?.plan && Data?.plan?.status !== "active") ||
+          (Data?.plan &&
+            Data?.plan?.status !== "active" &&
+            Data?.totalSecondsAvailable <= 120) ||
           (Data?.plan &&
             Data?.plan?.status == "active" &&
             Data?.totalSecondsAvailable <= 120)
