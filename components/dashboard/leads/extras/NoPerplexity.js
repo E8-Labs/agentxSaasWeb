734e08883919704f6fe65ec5bc8246bcee1c3972
HamzaLatif8712 <hamza1958712@gmail.com>
2025-05-19 19:21:42 +0500

    enrich leads cost update

--- components/dashboard/leads/extras/NoPerplexity.js
+++ components/dashboard/leads/extras/NoPerplexity.js
@@ -63,7 +63,7 @@ function NoPerplexity({
             <div className="flex flex-row gap-2 items-center">
 
                 <div style={{ fontSize: 13, fontWeight: '500', color: '#00000060', }}>
-                    credit cost ($0.10/lead)
+                    credit cost ($0.05/lead)
                 </div>
 
                 <Tooltip
