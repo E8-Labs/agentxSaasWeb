bd46e4f49ab1593b4ff6d381be7e065ba06c7743
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-06-12 22:04:41 +0500

    assign number

--- components/admin/users/AdminAgentX.js
+++ components/admin/users/AdminAgentX.js
@@ -3781,8 +3781,8 @@ function AdminAgentX({ selectedUser, from }) {
                                   value={showGlobalBtn ? 16505403715 : ""}
                                   // disabled={!showGlobalBtn}
                                   disabled={
-                                    assignNumber?.replace("+", "") ===
-                                    Constants.GlobalPhoneNumber.replace("+", "")
+                                    (assignNumber && assignNumber.replace("+", "") === Constants.GlobalPhoneNumber.replace("+", "")) ||
+                                    (showDrawerSelectedAgent && showDrawerSelectedAgent.agentType === "inbound")
                                   }
                                   onClick={() => {
                                     console.log(
