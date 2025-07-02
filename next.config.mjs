428c67b1105143a9b1c899372cdf5aaed733546b
HamzaLatif8712 <hamza1958712@gmail.com>
2025-07-03 02:19:38 +0500

    sentry auth token update

--- next.config.mjs
+++ next.config.mjs
@@ -1,73 +1,85 @@
-import {withSentryConfig} from '@sentry/nextjs';
+import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'www.blindcircle.com',
-            // port: '444',  // Specify the port here
-            pathname: '/agentx/uploads/**',  // Allow specific path
-          },
-          {
-            protocol: 'https',
-            hostname: 'www.blindcircle.com',
-            // port: '444',  // Specify the port here
-            pathname: '/agentxtest/uploads/**',  // Allow specific path
-          },
-          {
-            protocol: 'https',
-            hostname: 'iggi-media.s3.amazonaws.com',  // Another domain you're using
-          },
-          {
-            protocol: 'https',
-            hostname: 'images.unsplash.com',
-          },
-          {
-
-            protocol: 'https',
-            hostname: 'apimyagentx.com',
-          },
-          {
-
-            protocol: 'https',
-            hostname: 'api.myagentx.com',
-          },
-
-          
-        ],
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.blindcircle.com',
+        // port: '444',  // Specify the port here
+        pathname: '/agentx/uploads/**',  // Allow specific path
       },
+      {
+        protocol: 'https',
+        hostname: 'www.blindcircle.com',
+        // port: '444',  // Specify the port here
+        pathname: '/agentxtest/uploads/**',  // Allow specific path
+      },
+      {
+        protocol: 'https',
+        hostname: 'iggi-media.s3.amazonaws.com',  // Another domain you're using
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+
+        protocol: 'https',
+        hostname: 'apimyagentx.com',
+      },
+      {
+
+        protocol: 'https',
+        hostname: 'api.myagentx.com',
+      },
+
+
+    ],
+  },
 };
 
-export default withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://www.npmjs.com/package/@sentry/webpack-plugin#options
+const isSentryEnabled = process.env.SENTRY_AUTH_TOKEN;
+export default isSentryEnabled
+  ? withSentryConfig(nextConfig, {
+    org: "e8labs",
+    project: "javascript-nextjs",
+    silent: !process.env.CI,
+    widenClientFileUpload: true,
+    disableLogger: true,
+    automaticVercelMonitors: true,
+  })
+  : nextConfig;
 
-org: "e8labs",
-project: "javascript-nextjs",
+// export default withSentryConfig(nextConfig, {
+// // For all available options, see:
+// // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
+// org: "e8labs",
+// project: "javascript-nextjs",
 
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+// // Only print logs for uploading source maps in CI
+// silent: !process.env.CI,
 
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+// // For all available options, see:
+// // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-// Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-// tunnelRoute: "/monitoring",
+// // Upload a larger set of source maps for prettier stack traces (increases build time)
+// widenClientFileUpload: true,
 
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
+// // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+// // This can increase your server load as well as your hosting bill.
+// // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+// // side errors will fail.
+// // tunnelRoute: "/monitoring",
 
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
-});
+// // Automatically tree-shake Sentry logger statements to reduce bundle size
+// disableLogger: true,
+
+// // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+// // See the following for more information:
+// // https://docs.sentry.io/product/crons/
+// // https://vercel.com/docs/cron-jobs
+// automaticVercelMonitors: true,
+// });
