479a624b18c1facf00bbc66f025aa9e37917e942
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-07-05 16:11:19 +0500

    vapi transparent background widget

--- app/test-embed/page.js
+++ app/test-embed/page.js
@@ -1,9 +1,11 @@
 // app/test-embed/page.tsx
 "use client";
 
+import { useEffect } from "react";
+
 export default function TestEmbed() {
   return (
-    <div style={{ minHeight: "100vh", background: "red", }}>
+    <div style={{ minHeight: "100vh", background: "red" }}>
       <iframe
         src="/embed/vapi?assistantId=84ceab69-812d-4c6e-a8e1-0c8cb23f7a95"
         width="350"
@@ -15,7 +17,7 @@ export default function TestEmbed() {
           boxShadow: "0 2px 8px rgba(0,0,0,0.06)",
           position: "absolute",
           right: "2%",
-          bottom: "3%"
+          bottom: "3%",
         }}
         title="AgentX Widget"
       />
