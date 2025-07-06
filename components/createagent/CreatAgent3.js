95561b0541fa6972c57c69ffd0388665306379c7
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-07-07 01:39:51 +0500

    agree text removed

--- components/createagent/CreatAgent3.js
+++ components/createagent/CreatAgent3.js
@@ -756,7 +756,7 @@ const CreatAgent3 = ({
             </div>*/}
           {selectedPlan && (
             <div className="flex flex-col gap-2 absolute left-1/2 transform -translate-x-1/2 right-2 bottom-[3%] bg-white/30 backdrop-blur-lg w-full md:w-10/12 lg:w-5/12">
-              <div className="flex flex-row items-center gap-4 justify-start w-full  mt-6 pb-4 hidden sm:flex ">
+              {/* <div className="flex flex-row items-center gap-4 justify-start w-full  mt-6 pb-4 hidden sm:flex ">
                 <button onClick={handleToggleTermsClick}>
                   {agreeTerms ? (
                     <div
@@ -778,9 +778,9 @@ const CreatAgent3 = ({
                   )}
                 </button>
                 <TermsText />
-              </div>
+              </div> */}
               <div className=" w-full flex-col items-center flex mt-2">
-                {selectedPlan && (screenWidth < 640 || agreeTerms) ? (
+                {selectedPlan ? (
                   <div className="w-full flex-col items-center flex">
                     {selectedPlan?.id > 1 ? (
                       <button
