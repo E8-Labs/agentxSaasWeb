43a243f591e482992b186cf0aa9de7ca4fc388fc
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-07-08 02:37:55 +0500

    test

--- components/createagent/CreatAgent3.js
+++ components/createagent/CreatAgent3.js
@@ -758,7 +758,7 @@ const CreatAgent3 = ({ handleContinue, smallTerms, user, handleBack, screenWidth
             <TermsText />
             </div>*/}
           {selectedPlan && (
-            <div className="flex flex-col gap-2 absolute left-1/2 transform -translate-x-1/2 right-2 bottom-[8%] bg-white/30 backdrop-blur-lg w-full md:w-10/12 lg:w-5/12">
+            <div className="flex flex-col gap-2 absolute left-1/2 transform -translate-x-1/2 right-2 bottom-[3%] bg-white/30 backdrop-blur-lg w-full md:w-10/12 lg:w-5/12">
               {/* <div className="flex flex-row items-center gap-4 justify-start w-full  mt-6 pb-4 hidden sm:flex ">
                 <button onClick={handleToggleTermsClick}>
                   {agreeTerms ? (
@@ -813,7 +813,7 @@ const CreatAgent3 = ({ handleContinue, smallTerms, user, handleBack, screenWidth
                           setAddPaymentPopUp(true);
                         }}
                       >
-                        Claim 30 mins
+                        Claim 30 Mins
                       </button>
                     )}
                   </div>
@@ -880,31 +880,31 @@ const CreatAgent3 = ({ handleContinue, smallTerms, user, handleBack, screenWidth
                   </button>
                 </div>
                 <div className="text-center mt-2 text-[18px] font-[700] md:text-[24px] md:font-[700]">
-                  {/* {selectedPlan?.id > 1
+                  {selectedPlan?.id > 1
                     ? "Add your payment method" //"Select a plan that fits your needs"
-                    : "Start for free. Then pay as you go!"} */}
-                  You will not be charged today
+                    : "You will not be charged today"}
+                  {/* You will not be charged today */}
                 </div>
 
-                {/* {selectedPlan?.id > 1 ? (
+                {selectedPlan?.id > 1 ? (
                   <div
                     className="text-center mt-4 md:text-[17px] md:font-[600]" //style={styles.headingStyle}
                   >
                     Your minutes will renew monthly or after using{" "}
                     {selectedPlan?.mints} mins
                   </div>
-                ) : ( */}
+                ) : (
                 <div
-                  className="text-center test-14 font-[500] mt-4 md:text-[17px] md:font-[500]"
+                  className="text-center test-14 font-[500] mt-4 sm:text-[17px] sm:font-[500]"
                   style={{
                     // fontSize: 16,
                     // fontWeight: "600",
                     color: "#00000060",
                   }}
                 >
-                  Payment starts after 7 days or 30 min of Al usage
+                  Payment starts after 7 days or 30 min of AI usage
                 </div>
-                {/* )} */}
+                )}
 
                 <Elements stripe={stripePromise}>
                   <AddCardDetails
