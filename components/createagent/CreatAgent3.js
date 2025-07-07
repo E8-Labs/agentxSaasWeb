8e868c340c85f3c55943354020fde9a8346a4e7a
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-07-07 21:43:41 +0500

    payment title updated

--- components/createagent/CreatAgent3.js
+++ components/createagent/CreatAgent3.js
@@ -22,7 +22,13 @@ let stripePublickKey =
 // //console.log;
 const stripePromise = loadStripe(stripePublickKey);
 
-const CreatAgent3 = ({ handleContinue, smallTerms, user, handleBack, screenWidth }) => {
+const CreatAgent3 = ({
+  handleContinue,
+  smallTerms,
+  user,
+  handleBack,
+  screenWidth,
+}) => {
   const router = useRouter();
   const [togglePlan, setTogglePlan] = useState(false);
   const [selectedPlan, setSelectedPlan] = useState(null);
@@ -46,12 +52,11 @@ const CreatAgent3 = ({ handleContinue, smallTerms, user, handleBack, screenWidth
     }
   }, [togglePlan, agreeTerms]);
 
-
   useEffect(() => {
     const handlePopState = () => {
       console.log("🔙 Back gesture or browser back triggered");
       // if(window.screenWidth < 640){
-      handleBack()
+      handleBack();
       // }
     };
 
@@ -384,6 +389,12 @@ const CreatAgent3 = ({ handleContinue, smallTerms, user, handleBack, screenWidth
       outline: "none",
     },
   };
+  const windowWidth = () => {
+    if (typeof window !== "undefined") {
+      console.log("Window width is", window.innerWidth);
+      return window.innerWidth;
+    }
+  };
 
   return (
     <div
@@ -709,6 +720,11 @@ const CreatAgent3 = ({ handleContinue, smallTerms, user, handleBack, screenWidth
                     </div>
                   </div>
                 </div>
+                <div
+                  className="mt-2 text-start w-full md:w-10/12 lg:w-6/12"
+                  style={{ color: "#151515", fontSize: windowWidth() < 640 ? 13 : 16, fontWeight: "600" }}>
+                  Calls volume are projects based on current call volume.
+                </div>
               </div>
 
               <div
@@ -750,8 +766,8 @@ const CreatAgent3 = ({ handleContinue, smallTerms, user, handleBack, screenWidth
             <TermsText />
             </div>*/}
           {selectedPlan && (
-            <div className="flex flex-col gap-2 absolute left-1/2 transform -translate-x-1/2 right-2 bottom-[3%] bg-white/30 backdrop-blur-lg w-full md:w-10/12 lg:w-5/12">
-              <div className="flex flex-row items-center gap-4 justify-start w-full  mt-6 pb-4 hidden sm:flex ">
+            <div className="flex flex-col gap-2 absolute left-1/2 transform -translate-x-1/2 right-2 bottom-[5%] bg-white/30 backdrop-blur-lg w-full md:w-10/12 lg:w-5/12">
+              {/* <div className="flex flex-row items-center gap-4 justify-start w-full  mt-6 pb-4 hidden sm:flex ">
                 <button onClick={handleToggleTermsClick}>
                   {agreeTerms ? (
                     <div
@@ -773,9 +789,9 @@ const CreatAgent3 = ({ handleContinue, smallTerms, user, handleBack, screenWidth
                   )}
                 </button>
                 <TermsText />
-              </div>
-              <div className=" w-full flex-col items-center flex mt-2">
-                {selectedPlan && (screenWidth < 640 || agreeTerms) ? (
+              </div> */}
+              <div className=" w-full flex-col items-center flex">
+                {selectedPlan ? (
                   <div className="w-full flex-col items-center flex">
                     {selectedPlan?.id > 1 ? (
                       <button
@@ -872,30 +888,29 @@ const CreatAgent3 = ({ handleContinue, smallTerms, user, handleBack, screenWidth
                   </button>
                 </div>
                 <div className="text-center mt-2 text-[18px] font-[700] md:text-[24px] md:font-[700]">
-                  {selectedPlan?.id > 1
+                  {/* {selectedPlan?.id > 1
                     ? "Add your payment method" //"Select a plan that fits your needs"
-                    : "Start for free. Then pay as you go!"}
+                    : "Start for free. Then pay as you go!"} */}
+                  You will not be charged today
                 </div>
 
-                {selectedPlan?.id > 1 ? (
+                {/* {selectedPlan?.id > 1 ? (
                   <div
                     className="text-center mt-4 md:text-[17px] md:font-[600]" //style={styles.headingStyle}
                   >
                     Your minutes will renew monthly or after using{" "}
                     {selectedPlan?.mints} mins
                   </div>
-                ) : (
+                ) : ( */}
                   <div
-                    className="text-center mt-4 md:text-[17px] md:font-[700]"
+                    className="text-center mt-4 text-[14px] font-[500] md:text-[16px] md:font-[500]"
                     style={{
-                      fontSize: 16,
-                      fontWeight: "600",
-                      color: "#151515",
+                      color: "#00000060",
                     }}
                   >
-                    Payment starts after your 7 day free trial*
+                    Payment starts after 7 days or 30 min of Al usage
                   </div>
-                )}
+                {/* )} */}
 
                 <Elements stripe={stripePromise}>
                   <AddCardDetails
@@ -906,8 +921,8 @@ const CreatAgent3 = ({ handleContinue, smallTerms, user, handleBack, screenWidth
                     handleClose={handleClose}
                     togglePlan={togglePlan}
                     textBelowContinue={`${selectedPlan?.mints === 30
-                      ? "Trial is limited to 30 mins"
-                      : ""
+                        ? "Trial is limited to 30 mins"
+                        : ""
                       }`}
                   // handleSubLoader={handleSubLoader} handleBuilScriptContinue={handleBuilScriptContinue}
                   />
@@ -980,19 +995,19 @@ const CreatAgent3 = ({ handleContinue, smallTerms, user, handleBack, screenWidth
                     const SM_SCREEN_SIZE = 640; // Tailwind's sm breakpoint is typically 640px
 
                     if (screenWidth <= SM_SCREEN_SIZE) {
-                      if (selectedPlan.id === 1) {
-                        setShowSubscribeplan2(true);
-                      } else {
-                        router.push("/createagent/desktop");
-                      }
+                      // if (selectedPlan.id === 1) {
+                      //   setShowSubscribeplan2(true);
+                      // } else {
+                      router.push("/createagent/desktop");
+                      // }
                       // //console.log;
                     } else {
                       // //console.log;
-                      if (selectedPlan.id === 1) {
-                        setShowSubscribeplan2(true);
-                      } else {
-                        handleContinue();
-                      }
+                      // if (selectedPlan.id === 1) {
+                      //   setShowSubscribeplan2(true);
+                      // } else {
+                      handleContinue();
+                      // }
                     }
                   }}
                 >
@@ -1292,15 +1307,15 @@ const CreatAgent3 = ({ handleContinue, smallTerms, user, handleBack, screenWidth
 export default CreatAgent3;
 
 function TermsText() {
-
   const windowWidth = () => {
     if (typeof window !== "undefined") {
       console.log("Window width is", window.innerWidth);
       return window.innerWidth;
     }
-  }
+  };
 
   return (
+
     <div
       className="flex flex-row w-full items-center gap-1"
       style={{ color: "#151515", fontSize: windowWidth() < 640 ? 10 : 13, fontWeight: "600" }}
