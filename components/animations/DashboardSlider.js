5b604d1964f6b41cf326bcd2545e53d28bde416f
HamzaLatif8712 <hamza1958712@gmail.com>
2025-07-09 00:46:26 +0500

    add feedback link click update

--- components/animations/DashboardSlider.js
+++ components/animations/DashboardSlider.js
@@ -149,11 +149,7 @@ const DashboardSlider = ({
       // setShowAskSkyConfirmation(true);
       setShowAskSkyModal(true);
       setShouldStartCall(true);
-    } else if (item.id === 4) {
-      setShowVapiChatWidget(true);
-    }
-
-    else {
+    } else {
       if (typeof window !== "undefined") {
         window.open(item.url, "_blank");
       }
