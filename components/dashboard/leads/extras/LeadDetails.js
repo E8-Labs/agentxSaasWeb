b85cf058a2f7631ee73705dc1fc4779795392e91
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-07-16 02:23:56 +0500

    voicemail

--- components/dashboard/leads/extras/LeadDetails.js
+++ components/dashboard/leads/extras/LeadDetails.js
@@ -2180,35 +2180,29 @@ const LeadDetails = ({
                                                         alt="*"
                                                       />
                                                     </button>
-                                                    {item.agent.hasVoicemail ? (
-                                                      <div>
-                                                        {item.voicemailsent ? (
-                                                          <NoVoicemailView
-                                                            showAddBtn={false}
-                                                            title={
-                                                              "Voicemail Delivered"
-                                                            }
-                                                            subTitle={
-                                                              "Delivered during the first missed call"
-                                                            }
-                                                          />
-                                                        ) : (
-                                                          <NoVoicemailView
-                                                            showAddBtn={false}
-                                                            title={
-                                                              "Not able to Leave a Voicemail"
-                                                            }
-                                                            subTitle={
-                                                              "The phone was either a landline or has a full voicemail"
-                                                            }
-                                                          />
-                                                        )}
-                                                      </div>
+                                                     {item.agent.hasVoicemail ? (
+
+                                                      <NoVoicemailView
+                                                        showAddBtn={false}
+                                                        title={
+                                                          "Voicemail Delivered"
+                                                        }
+                                                        subTitle={
+                                                          "Delivered during the first missed call"
+                                                        }
+                                                      />
                                                     ) : (
                                                       <NoVoicemailView
                                                         showAddBtn={false}
+                                                        title={
+                                                          "Not able to Leave a Voicemail"
+                                                        }
+                                                        subTitle={
+                                                          "The phone was either a landline or has a full voicemail"
+                                                        }
                                                       />
                                                     )}
+
                                                   </div>
                                                 ) : (
                                                   <>
