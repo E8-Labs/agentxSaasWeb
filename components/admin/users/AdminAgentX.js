35a71ae24f7976fcba8acb884899799256c4546a
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-08-02 23:49:37 +0500

    action title update

--- components/admin/users/AdminAgentX.js
+++ components/admin/users/AdminAgentX.js
@@ -264,7 +264,7 @@ function AdminAgentX({ selectedUser, agencyUser, from }) {
 
   const [user, setUser] = useState(null);
 
-  let tabs = ["Agent Info", "Calendar", "Pipeline", "Knowledge"];
+  let tabs = ["Agent Info", "Actions", "Pipeline", "Knowledge"];
   const [AgentMenuOptions, setAgentMenuOptions] = useState(tabs);
 
   const [preview, setPreview] = useState(null);
@@ -3984,7 +3984,7 @@ function AdminAgentX({ selectedUser, agencyUser, from }) {
                     />
                   </div>
                 </div>
-              ) : activeTab === "Calendar" ? (
+              ) : activeTab === "Actions" ? (
                 <div>
                   <div
                     className=" lg:flex hidden  xl:w-[350px] lg:w-[350px]"
