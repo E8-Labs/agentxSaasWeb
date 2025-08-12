05ccbb2bf61453f9c04f2bce4048d02f1420fe57
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-08-12 20:55:02 +0500

    save updates

--- components/pipeline/tagInputs/PromptTagInput.js
+++ components/pipeline/tagInputs/PromptTagInput.js
@@ -593,7 +593,7 @@ export const PromptTagInput = ({
                     style={{ fontWeight: "600", fontSize: 15 }}
                     onClick={() => {
                       setShowScriptModal(false);
-                      saveUpdates();
+                      // saveUpdates();
                     }}
                   >
                     Save Changes
