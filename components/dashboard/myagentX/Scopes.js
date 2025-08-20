ecb4bac9f72024647bca5d881fec1c34de121aa8
HamzaLatif8712 <hamza1958712@gmail.com>
2025-08-20 15:32:08 +0500

    google calendar scopes updated

--- components/dashboard/myagentX/Scopes.js
+++ components/dashboard/myagentX/Scopes.js
@@ -3,6 +3,8 @@ export const Scopes = [
     "https://www.googleapis.com/auth/calendar.calendars.readonly",
     "https://www.googleapis.com/auth/calendar.events",
     "https://www.googleapis.com/auth/calendar.events.owned",
+    "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/gmail.readonly"
     // "https://www.googleapis.com/auth/calendar"
 ]
 
