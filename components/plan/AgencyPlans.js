507c1787078a380b7ec00597a8307fff9fc22e72
HamzaLatif8712 <hamza1958712@gmail.com>
2025-09-09 00:42:55 +0500

    small fix for agecy prices

--- components/plan/AgencyPlans.js
+++ components/plan/AgencyPlans.js
@@ -711,7 +711,7 @@ function AgencyPlans() {
                                                         </div>
 
                                                         <div className='text-center ' style={{ fontSize: 15, fontWeight: '500' }}>
-                                                            ${item.ratePerMin} per min
+                                                            ${item?.ratePerMin?.toFixed(2)} per min
                                                         </div>
 
                                                         <div className="mt-3 mb-3">
