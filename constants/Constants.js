ac87e05628896752a2cfdf23e9ceb2561d43c444
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-11-08 00:30:43 +0500

    fix: update privacy policy URL in Constants.js to reflect new domain for improved accuracy

--- constants/Constants.js
+++ constants/Constants.js
@@ -249,7 +249,7 @@ export const callStatusColors = {
 
 export const termsAndConditionUrl = "https://www.myagentx.com/terms-and-condition";
 
-export const privacyPollicyUrl = "https://www.myagentx.com/privacy-policy";
+export const privacyPollicyUrl = "https://www.assignx.ai/cancellation-return-policy"//"https://www.assignx.ai/privacy-policy"//"https://www.myagentx.com/privacy-policy";
 export const CancellationAndRefundUrl = "https://www.google.com/url?q=https://www.myagentx.com/cancellation-return-policy&sa=D&source=docs&ust=1751402335347404&usg=AOvVaw2aDueiwwhLkomUAiT_eTPT";
 export const next30Days = moment().add(30, "days").format("MM/DD/YYYY");
 
@@ -257,7 +257,6 @@ export const next30Days = moment().add(30, "days").format("MM/DD/YYYY");
 export const ScrollBarCss = () => {
   return "scrollbar scrollbar-track-scrollBarPurple scrollbar-thin scrollbar-thumb-transparent"
 }
-
 
 export const allIntegrations = [
   {
