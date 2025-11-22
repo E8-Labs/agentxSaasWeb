dded7df11a4ee5b2481b601f4cbe3f541150e009
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-11-22 20:25:53 +0500

    refactor: clean up imports and improve code consistency in ProfileNav and AgencyNavBar components
    - Removed unused imports and consolidated import statements for better readability.
    - Standardized string quotes and formatting across the ProfileNav component.
    - Eliminated unnecessary padding in the AgencyNavBar component for cleaner UI.
    
    These changes enhance code maintainability and improve overall consistency in the project.

--- components/dashboard/Navbar/AgencyNavBar.js
+++ components/dashboard/Navbar/AgencyNavBar.js
@@ -652,7 +652,6 @@ const AgencyNavBar = () => {
                       style={{
                         fontSize: 15,
                         fontWeight: 500, //color: pathname === item.href ? "#402FFF" : 'black'
-                        paddingLeft: item.name === 'Activity' ? '5px' : '0px',
                       }}
                     >
                       {item.name}
