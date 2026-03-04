4d59c31ffc7399aa6f328e0c9cebbe1933e6e7ce
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2026-03-04 16:49:24 +0500

    Enhance SelectedUserDetails component with Lucide icons and update AgencySupportWidget for support webinar
    - Added Lucide icons to the SelectedUserDetails component for improved visual representation of menu items.
    - Updated the AgencySupportWidget to replace the 'Speak to a Geek' option with a 'Support Webinar' link, enhancing user support options.
    - Adjusted the onlyCountries prop in Teams component to include additional countries (GB, AU) for better international support.

--- constants/Constants.js
+++ constants/Constants.js
@@ -74,6 +74,7 @@ export const PersistanceKeys = {
   AgencyPartnerUrl: 'https://set.myagentx.com/widget/bookings/assignx-gameplan-session',
   AgencyBillingSupportUrl: 'https://forms.clickup.com/8691504/f/897tg-14437/N7OCXKEV9N8ST83P9D',
   AgencySupportTicketUrl: 'https://forms.clickup.com/8691504/f/897tg-14237/Z94ZMZJ4UJ4W8B9MHK',
+  AgencySupportWebinarUrl: 'https://www.google.com/url?q=https://api.leadconnectorhq.com/widget/bookings/support-calendar-assignx&sa=D&source=docs&ust=1772560308932327&usg=AOvVaw1ib2CP_FD0_B0_tRZc6QyE',
   // Default Script Builder URL (used when agency has not set a custom one for subaccounts)
   DefaultScriptBuilderUrl: 'https://chatgpt.com/g/g-0O0jItKdk-agentx-script-builder',
   // Main User URLs
